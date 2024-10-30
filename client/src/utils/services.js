baa90ab356825e8b8370ee7f635ed827fe7cd25d
Ahmad <eng.ahmed.tahan2@gmail.com>
2024-10-30 12:34:18 +0300

    change baseUrl

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,4 +1,7 @@
-export const baseUrl = "http://localhost:5000/api";
+//export const baseUrl = "http://localhost:5000/api";
+export const baseUrl = "https://realtime-chat-api-oe1p.onrender.com/api";
+
+
 export const postRequest = async(url, body) => {
     const response = await fetch(url, {
         method: "POST",
